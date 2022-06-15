7c00bca3d6d6104d40532f3432e733ba89e123a1
Viola Brauns <90695338+vdbrauns@users.noreply.github.com>
2022-06-15 23:35:16 +0300

    .

---
+++ js/js.js
@@ -0,0 +1,65 @@
+$(document).ready(function(){
+$(".first").mouseover(function(){
+$(".child").css("color", "white");
+$(".des").css("color", "white");
+});
+$(".first").mouseout(function(){
+$(".child").css("color", "#010BFF");
+$(".des").css("color", "#010BFF");
+});
+});
+
+$(document).ready(function(){
+$(".two").mouseover(function(){
+$(".career").css("color", "white");
+$(".desone").css("color", "white");
+});
+$(".two").mouseout(function(){
+$(".career").css("color", "#010BFF");
+$(".desone").css("color", "#010BFF");
+});
+});
+
+$(document).ready(function(){
+$(".three").mouseover(function(){
+$(".world").css("color", "white");
+$(".desthree").css("color", "white");
+});
+$(".three").mouseout(function(){
+$(".world").css("color", "#010BFF");
+$(".desthree").css("color", "#010BFF");
+});
+});
+
+$(document).ready(function(){
+$(".four").mouseover(function(){
+$(".tehnolog").css("color", "white");
+$(".desfour").css("color", "white");
+});
+$(".four").mouseout(function(){
+$(".tehnolog").css("color", "#010BFF");
+$(".desfour").css("color", "#010BFF");
+});
+});
+
+$(document).ready(function(){
+$(".five").mouseover(function(){
+$(".univ").css("color", "white");
+$(".desfive").css("color", "white");
+});
+$(".five").mouseout(function(){
+$(".univ").css("color", "#010BFF");
+$(".desfive").css("color", "#010BFF");
+});
+});
+
+$(document).ready(function(){
+$(".six").mouseover(function(){
+$(".silence").css("color", "white");
+$(".dessix").css("color", "white");
+});
+$(".six").mouseout(function(){
+$(".silence").css("color", "#010BFF");
+$(".dessix").css("color", "#010BFF");
+});
+});
